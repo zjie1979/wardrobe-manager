b5eea3015b0f108cba99d39b18ed99f7a404244f
zjie1979 <13360694@qq.com>
2026-08-15 00:13:51 +0800

    fix: improve wardrobe mobile data reliability

--- sw.js
+++ sw.js
@@ -1,13 +1,26 @@
-const CACHE_NAME = 'wardrobe-v4';
+const CACHE_NAME = 'wardrobe-v5';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('fetch', event => {
+  if(event.request.method !== 'GET') return;
   event.respondWith(
-    caches.match(event.request).then(r => r || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        if(response && response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
 
@@ -15,6 +28,6 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(names => Promise.all(
       names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
-    ))
+    )).then(() => self.clients.claim())
   );
 });
